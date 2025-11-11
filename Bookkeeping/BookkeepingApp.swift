7616ee6f024f20768a568fea430f8c23008907d8
LW <568270833@qq.com>
2025-11-11 14:31:45 +0800

    Initial Commit

---
+++ Bookkeeping/BookkeepingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookkeepingApp.swift
+//  Bookkeeping
+//
+//  Created by 王敬霖 on 2025/11/11.
+//
+
+import SwiftUI
+
+@main
+struct BookkeepingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookkeeping/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bookkeeping
+//
+//  Created by 王敬霖 on 2025/11/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
